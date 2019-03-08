3fc967e319d99b960b291898f7288a4d3dac9adf
Ivan <ivan.ropiteaux@gmail.com>
2019-03-08 10:29:46 +0100

    add cors support

--- app/core/router.js
+++ app/core/router.js
@@ -17,7 +17,7 @@ server
     .use(restify.plugins.bodyParser())
     .use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization");
         return next();
     });
 
